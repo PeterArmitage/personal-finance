79f727be11ef239db34e5203e79e9932fd4923b7
Peter Armitage <ficochu84@gmail.com>
2024-09-05 21:13:30 -0300

    fixed issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
